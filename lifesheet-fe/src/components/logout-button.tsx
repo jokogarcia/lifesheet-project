1a395f63382c1aba4aa5e172d423888a07a3536a
Joaquin Garcia <joaquin.garcia@edugo.ai>
2025-08-10 20:43:28 +0200

    style logout button

--- lifesheet-fe/src/components/logout-button.tsx
+++ lifesheet-fe/src/components/logout-button.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
+import { Button } from "./ui/button";
+import { Briefcase, LogOutIcon } from "lucide-react";
 
 const LogoutButton = () => {
   const { logout } = useAuth0();
 
   return (
-    <button onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>
+    <Button variant="outline" style={{color: "red"}} onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>
+      <LogOutIcon className="h-4 w-4 mr-2" />
       Log Out
-    </button>
+    </Button>
   );
 };
 
