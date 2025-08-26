3090d48be4159228dcf6c519e94288395cf4155f
Joaquin Garcia <joaquin.garcia@edugo.ai>
2025-08-26 10:25:44 +0200

    feat: add StepProgressIndicator component with styling for progress tracking

---
+++ lifesheet-fe/src/components/ui/step-progress-indicator.tsx
@@ -0,0 +1,27 @@
+import moduleStyles from './step-progress-indicator.module.css'
+export interface StepProgressIndicatorProps {
+  steps: string[];
+  currentStep: number;
+  onStepClick?: (step: number) => void;
+}
+export const StepProgressIndicator = (props: StepProgressIndicatorProps) => {
+  const { steps, currentStep, onStepClick } = props;
+
+  return (
+    <div className={moduleStyles['progressbar-wrapper'] }>
+      <ul className={moduleStyles.progressbar}>
+        {steps.map((step, index) => {
+            let className ="";
+            if(!!onStepClick) className = moduleStyles.interactive;
+            if(index < currentStep) className = moduleStyles.previous;
+            if(index === currentStep) className = moduleStyles.active;
+          return (
+            <li key={index} className={className} onClick={() => !!onStepClick && onStepClick(index)}>
+              {step}
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
