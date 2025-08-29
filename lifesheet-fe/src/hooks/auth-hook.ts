569b6d90f9582cd0764248e5b84b25c0edaffd25
Joaquin Garcia <joaquin.garcia@edugo.ai>
2025-08-29 11:42:39 +0200

    refactor: optimize useAuth hook to utilize useMemo for performance improvements

--- lifesheet-fe/src/hooks/auth-hook.ts
+++ lifesheet-fe/src/hooks/auth-hook.ts
@@ -1,11 +1,12 @@
 import { useKeycloak } from "@react-keycloak/web";
+import { useMemo } from "react";
 /**
  * This hook is meant to provide a drop-in replacement for Auth0 useAuth hook
  * but using keycloak
  */
 export function useAuth() {
     const { keycloak } = useKeycloak();
-    return {
+    return useMemo(() => ({
         isAuthenticated: keycloak.authenticated,
         user: keycloak.tokenParsed,
         loginWithRedirect: keycloak.login,
@@ -19,6 +20,5 @@ export function useAuth() {
             }
             throw new Error("User is not authenticated");
         },
-
-    };
+    }), [keycloak]);
 }
