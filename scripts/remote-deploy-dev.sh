50ab3225aa3e77c413c79ea3c78f51b774e82f43
Joaquin Garcia <joaquin.garcia@edugo.ai>
2025-09-03 12:20:06 +0200

    fix: port number in dev deploy script

--- scripts/remote-deploy-dev.sh
+++ scripts/remote-deploy-dev.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 BRANCHNAME="${1:-master}"
-ssh quin@irazu.zapto.org "cd repos/lifesheet-project/scripts && bash self-deploy-dev.sh $BRANCHNAME"
+ssh -p 2907 quin@irazu.zapto.org "cd repos/lifesheet-project/scripts && bash self-deploy-dev.sh $BRANCHNAME"
