96638f5b9ebe25da369bff6c2d747313d56d99d2
Joaquin Garcia <joaquin.garcia@edugo.ai>
2025-09-05 12:46:16 +0200

    feat: revamp the Dashboard with big cards instead of a toolbar

--- lifesheet-fe/src/components/dashboard.tsx
+++ lifesheet-fe/src/components/dashboard.tsx
@@ -1,11 +1,10 @@
 import { useNavigate } from 'react-router-dom';
-import TailoredCVs from './tailored-cvs';
-import { Button } from './ui/button';
-import { Award, Download, LogOut, UserRoundPen } from 'lucide-react';
 import { useAuth } from '@/hooks/auth-hook';
 import { useUserCV } from '@/hooks/use-cv';
 import { LoadingIndicator } from './ui/loading-indicator';
 import { isCVOnboarded } from '@/services/cvs-service';
+import { Card } from '@radix-ui/themes';
+import { CardContent, CardFooter, CardHeader } from './ui/card';
 
 export function Dashboard() {
 
@@ -21,25 +20,39 @@ export function Dashboard() {
     return <LoadingIndicator />;
   }
   return (
-    <>
-      <div className="flex flex-row-reverse">
-        <Button onClick={() => logout()} variant="outline" className="btn-custom">
-          <LogOut className="h-4 w-4 mr-2" />
-        </Button>
-        <Button onClick={() => navigate('/plans')} variant="outline" className="btn-custom">
-          <Award className="h-4 w-4 mr-2" />
-        </Button>
-        <Button onClick={() => navigate('/cv-data')} variant="outline" className="btn-custom">
-          <UserRoundPen className="h-4 w-4 mr-2" />
-        </Button>
-        <Button onClick={() => navigate('/export-pdf')} variant="outline" className="btn-custom">
-          <Download className="h-4 w-4 mr-2" />
-          Export my CV
-        </Button>
+    <><header className="mb-8">
+      <div className="flex flex-col items-center">
+        <h1 className="text-4xl text-gradient mb-2">Welcome, {cv?.personal_info.fullName || 'User'}</h1>
+        <p className="text-muted-foreground">Manage your CVs, subscriptions, and more</p>
       </div>
-      <div>
-        <TailoredCVs />
+    </header>
+      {/* Dashboard Cards */}
+      <div className="flex mt-16 mb-8 flex-wrap justify-center" >
+        <DashboardCard title="Export my CV" img='export.png' bottomText="Export your full, untailored CV as a PDF file." onClick={() => navigate('/export-pdf')} />
+        <DashboardCard title="Edit CV Data" img='edit-cv.png' bottomText="Edit your Personal Information, Work Experience, Education, Skills, or add a Profile Photo." onClick={() => navigate('/cv-data')} />
+        <DashboardCard title="CV Tailoring" img='tailor-cv.png' bottomText="Tailor a CV to a job, based on the job description and requirements." onClick={() => navigate('/tailor-cv')} />
+        <DashboardCard title="Tailored CVs" img='doc-list.png' bottomText="View and manage your tailored CVs." onClick={() => navigate('/tailored-cvs')} />
+        <DashboardCard title="My Subscription" img='subscriptions.png' bottomText="View or change your current subscription." onClick={() => navigate('/plans')} />
+        <DashboardCard title="Sign out" img='logout.png' bottomText="Sign out of your account." onClick={() => logout()} />
       </div>
+
+
     </>
+  );
+}
+function DashboardCard({ title, img, bottomText, onClick }: { title: string; img: string; bottomText: string; onClick: () => void }) {
+  return (
+    <Card className='flex flex-col border-2 card-hover cursor-pointer w-75 m-2 ' onClick={onClick}>
+      <CardHeader>
+        <h2 className='text-lg font-semibold'>{title}</h2>
+      </CardHeader>
+      <CardContent>
+        <img src={img} alt={title} className='h-auto w-30 mx-auto mb-2' />
+      </CardContent>
+      <CardFooter className='mt-auto p-4 pt-0'>
+        <p className='text-sm text-muted-foreground align-middle text-center'>{bottomText}</p>
+      </CardFooter>
+
+    </Card>
   );
 }
--- lifesheet-fe/src/components/tailor-cv.tsx
+++ lifesheet-fe/src/components/tailor-cv.tsx
@@ -8,6 +8,7 @@ import { useNavigate } from 'react-router-dom';
 import cvsService from '@/services/cvs-service';
 import { useSaaSActiveSubscription } from '@/hooks/use-saas';
 import RichTextEditor from './ui/editor';
+import { LoadingIndicator } from './ui/loading-indicator';
 
 export function TailorCV() {
   const { cv, isLoading } = useUserCV();
@@ -53,7 +54,7 @@ export function TailorCV() {
       <div className="min-h-screen flex items-center justify-center">
         <div className="text-center">
           <div className="animate-spin rounded-full h-8 w-8 border-b-2 border-gray-900 mx-auto"></div>
-          <p className="mt-2">Loading your CV data...</p>
+          <LoadingIndicator />
         </div>
       </div>
     );
