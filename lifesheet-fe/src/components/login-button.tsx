8406bbbef1ae526cbb3f423ba87d7a0bf4ad43b4
Joaquin Garcia <joaquin.garcia@edugo.ai>
2025-09-15 19:17:34 +0200

    delete unused components

--- lifesheet-fe/src/components/login-button.tsx
+++
@@ -1,8 +0,0 @@
-import { useKeycloak } from '@react-keycloak/web';
-
-const LoginButton = () => {
-  const { keycloak } = useKeycloak();
-  return <button onClick={() => keycloak?.login()}>Log In</button>;
-};
-
-export default LoginButton;
